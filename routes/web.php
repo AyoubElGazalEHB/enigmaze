38bcf8fafca38de366db042474b40064e2726f1f
AyoubElGazalEHB <126098600+AyoubElGazalEHB@users.noreply.github.com>
2024-01-08 02:36:01 +0100

    Changes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Homecontroller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get("/home",[HomeController::class,"index"]);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });

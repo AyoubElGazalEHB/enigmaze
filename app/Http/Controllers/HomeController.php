5733a93d29fbf35ed9d20ebc0d8f4b579371049c
AyoubElGazalEHB <126098600+AyoubElGazalEHB@users.noreply.github.com>
2024-01-08 18:13:11 +0100

    dashboard admin template

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
+
 class HomeController extends Controller
 {
     public function index(){
@@ -17,7 +20,7 @@ public function redirects(){
 
     if($usertype== '1')
     {
-       return view('home');
+       return view('admin.adminhome');
     }
 
     else{
